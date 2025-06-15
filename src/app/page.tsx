4faddd866353d4e4afe9c7620662436dd5504dc0
indrakumar <indrakumarhn01@gmail.com>
2025-06-15 19:02:11 +0530

    added the navbar section and root page with search feature

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Link href="/documents/123" className="underline text-blue-700">Click</Link>
-    </div>
-  );
-}
